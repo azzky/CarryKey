80de47eead511b2def9b63c15a31f8b0588d4388
Nikita Kulikov <nikita.kulikov@ewave.com>
2023-07-27 17:10:05 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Carrykey`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "0fm2e2eewL13A6UVnD-kq1Mq0qzBcYh42OqwqrhHfHU",
+      "spaceId": ""
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
