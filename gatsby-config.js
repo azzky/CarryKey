cc13c17f14f4e1b21add7f957e77111e1a07116a
Nikita Kulikov <nikita.kulikov@ewave.com>
2023-07-28 19:04:31 +0200

    fix leak

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@ module.exports = {
   plugins: [{
     resolve: 'gatsby-source-contentful',
     options: {
-      "accessToken": "0fm2e2eewL13A6UVnD-kq1Mq0qzBcYh42OqwqrhHfHU",
-      "spaceId": "3mb1yc1vta0f"
+      "accessToken": process.env.CONTENTFUL_ACCESS_TOKEN,
+      "spaceId": process.env.CONTENTFUL_SPACE_ID
     }
   }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", {
     resolve: 'gatsby-source-filesystem',
