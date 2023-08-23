02506331c2d792f3751fbc953a0b64c6333afbb2
azzky <azzky-demiurg@mail.ru>
2023-08-23 09:36:39 +0200

    search added

--- src/components/header/search.js
+++ src/components/header/search.js
@@ -1,11 +1,18 @@
 import React from "react"
+import { useForm } from "react-hook-form"
+import { navigate } from "gatsby"
+
 import SearchWrapper from './search.styled'
 
 const Search = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data) => {
+        navigate(`/search?search=${data.search}`)
+    }
     return (
-        <SearchWrapper>
-            <input type="text" placeholder='Search'/>
-            <button>
+        <SearchWrapper onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder='Search' {...register("search", { required: true })}/>
+            <button type="submit">
                 <svg width="26" height="26" viewBox="0 0 26 26" fill="none">
                     <use href="#search"/>
                 </svg>
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import Layout from '@components/layout'
+import ShopITems from '@hooks/useShopitems'
+import Grid from '@components/shopPage/grid';
 
-const Search = () => {
+const Search = ({location}) => {
+    const posts = ShopITems()
+    const string = location.search.split('?search=')[1] || 'empty'
+    let arr = []
+    posts.map(post => {
+        if(
+            string !== 'empty' && (
+                post.title.toLowerCase().includes(string.toLowerCase()) ||
+                post.categories?.includes(string.toLowerCase()) ||
+                post.tags?.includes(string.toLowerCase())
+            )
+        ) {
+            arr.push(post)
+        }
+        return null
+    })
     return (
         <Layout hasNavigation isHero
-            h1={'Search'}>
+            h1={'Search results for: '+ string}>
+            {arr?.length > 0 ? <Grid items={arr}/> : <h2>Nothing found</h2>}
         </Layout>
     )
 }
