3bd6f13b2e2516dd94bc03f898f362c91f1773c2
azzky <azzky-demiurg@mail.ru>
2024-05-15 14:03:40 +0200

    sorting restored

--- src/components/tags/index.js
+++ src/components/tags/index.js
@@ -7,8 +7,8 @@ import Wrapper from './tags.styled';
 export const Tags = ({tags, path, setSortingValue, sortList, selectedTag, sortOption}) => (
     <Wrapper className="top">
         <div className="tags">
-            {tags.length > 0 && tags.map(tag=>(<Link className="tag" key={tag} to={'/' +path +'/tag/'+tag.replace(' ','_')}>{tag}</Link>))}
-            {tags.length === 0 && <Link className='back' to='/shop'>{selectedTag+' x'}</Link>}
+            {/* {tags.length > 0 && tags.map(tag=>(<Link className="tag" key={tag} to={'/' +path +'/tag/'+tag.replace(' ','_')}>{tag}</Link>))} */}
+            {/* {tags.length === 0 && <Link className='back' to='/shop'>{selectedTag+' x'}</Link>} */}
         </div>
         <div className="sorting">
             <Select options={sortList} unstyled isSearchable={false}
