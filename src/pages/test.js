1eaf026054450e2cca1e84228826bb752dffff27
azzky <azzky-demiurg@mail.ru>
2024-06-05 10:00:57 +0200

    try fix resend error

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,16 +1,32 @@
 import React from "react"
 import { Resend } from 'resend';
 
-const Test = () => {
-    const resend = new Resend(process.env.GATSBY_RESEND_KEY);
+const resend = new Resend(process.env.GATSBY_RESEND_KEY);
 
+const Test = () => {
     const handler = async () => {
-        await resend.emails.send({
-            from: 'orders@carrykey.me',
-            to: 'azzky.demiurg@gmail.com',
-            subject: 'Hello World',
-            html: 'test'
-        });
+        try {
+            const { data, error } = await resend.emails.send({
+                from: 'orders@carrykey.me',
+                to: 'azzky.demiurg@gmail.com',
+                subject: 'Hello World',
+                html: '<p>test</p>'
+            });
+
+            if (error) {
+                return Response.json({ error }, { status: 500 });
+            }
+
+            return Response.json({ data });
+        } catch (error) {
+            return Response.json({ error }, { status: 500 });
+        }
+        // await resend.emails.send({
+        //     from: 'orders@carrykey.me',
+        //     to: 'azzky.demiurg@gmail.com',
+        //     subject: 'Hello World',
+        //     html: 'test'
+        // });
     }
     return <div>
         <button onClick={handler}>test!</button>
