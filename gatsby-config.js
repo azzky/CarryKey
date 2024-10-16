2f8619138ecf00942dd03c9b3311beaf29149b0e
azzky <azzky-demiurg@mail.ru>
2024-10-16 11:14:10 +0200

    try to fix sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -186,6 +186,9 @@ module.exports = {
                             page.path === '/shop/set/' + apiPage.url
                         );
                         const priority = page.path === "/" ? 1.0 : 0.7;
+                        if (page.path === '/') {
+                            page.path = ''
+                        }
                         const changefreq = page.path === "/" ? 'daily' : 'weekly';
                         const lastmod = matchingApiPage ? matchingApiPage.updatedAt : new Date().toISOString();
                         return { ...page, priority, changefreq, lastmod };
